862000c3ba961f2a1b23d81b0a14a6a81a3d5750
yoon <lrzrlove@naver.com>
2020-10-05 11:35:40 +0900

    Java Config 방식 연결 완료

---
+++ src/test/java/mybatis_spring_study/config/ContextSqlSessionTest.java
@@ -0,0 +1,43 @@
+package mybatis_spring_study.config;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { ContextRoot.class })
+public class ContextSqlSessionTest {
+
+	protected static final Log log = LogFactory.getLog(ContextSqlSessionTest.class);
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println();
+	}
+
+	@Autowired
+	private SqlSessionFactory sessionFactory;
+
+	@Test
+	public void testDataSource() throws SQLException {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		SqlSession session = sessionFactory.openSession();
+		log.debug("session " + session);
+		Assert.assertNotNull(sessionFactory);
+	}
+
+}
